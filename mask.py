cc79aafa8c17db37cebbf622b9a0141e2249b366
julian naufal <julian@naufal.com>
2022-06-20 09:25:02 +0700

    commit mask

---
+++ mask.py
@@ -0,0 +1,17 @@
+import cv2
+import uuid
+
+#images capture
+cap = cv2.VideoCapture(2)
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+while True:
+    ret, frame = cap.read()
+    imgname = '/home/mrjaz/Pictures/Webcam/{}.jpg'.format(str(uuid.uuid()))
+    cv2.imwrite(imgname, frame)
+    cv2.imshow('frame', frame)
+
+    if cv2.waitkey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cap.destroyAllwindows()
